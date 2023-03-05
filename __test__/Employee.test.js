a78980b97075a9d6e2aafdbf66869f4c11409a34
ttieman <tiemantanner@gmail.com>
2023-03-04 18:58:11 -0700

    got first working test done

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -4,10 +4,10 @@ const Employee = require('../lib/Employee');
   describe('Employee', () => {
     describe('Tests the name propery of Employee', () => {
 
-      it('Check the employee class to ensure the data is populated correctly'), () => {
+      it('Check the employee class to ensure the data is populated correctly', () => {
           const bob = new Employee("bob","14", "bobman@gmail.com");
-          expect(Employee.name).tobe("bob");
-      }
+          expect(bob.name).toBe("bob");
+      });
 
 
     });
